535e4b61af54c0d88d5142085c1ca86ad7877f56
Rampilla-Deepthi <nathravindra065@gmail.com>
2026-07-13 18:54:02 +0530

    Remove .env and add gitignore

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 load_dotenv(dotenv_path=".env")
 print("API KEY:", os.getenv("GEMINI_API_KEY"))
-genai.configure(api_key="AQ.Ab8RN6JjOvF6og4lDq_dptvbq1P2Eek6_dC2QmfqqiEyF7TZ4A")
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-3.5-flash")
 
 app.add_middleware(
